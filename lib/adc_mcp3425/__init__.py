267511a46a4c7fe80d72df19401f5609cb0f43a9
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-09-27 16:33:55 +0900

    0.0.1

--- lib/adc_mcp3425/__init__.py
+++ lib/adc_mcp3425/__init__.py
@@ -5,7 +5,7 @@ __prog_name__ = 'adc_mcp3425'
 __version__ = '0.0.1'
 __author__ = 'Yoichi Tanibayashi'
 
-from .adc_mcp3425 import MCP3425
+from .mcp3425 import MCP3425
 from .my_logger import get_logger
 
 all = ['MCP3425',
--- lib/adc_mcp3425/__main__.py
+++ lib/adc_mcp3425/__main__.py
@@ -1,6 +1,8 @@
 #
 # Copyright (c) 2021 Yoichi Tanibayashi
 #
+import time
+import pigpio
 import click
 from . import MCP3425
 from . import __prog_name__, __version__, __author__
@@ -10,17 +12,15 @@ from .my_logger import get_logger
 @click.group(invoke_without_command=True,
              context_settings=dict(help_option_names=['-h', '--help']),
              help=" by " + __author__)
-@click.option('--opt0', '-o0', 'opt0', type=str, default=None, help='opt1')
 @click.option('--debug', '-d', 'debug', is_flag=True, default=False,
               help='debug flag')
 @click.version_option(version=__version__)
 @click.pass_context
-def cli(ctx, opt0, debug):
+def cli(ctx, debug):
     """ command group """
     __log = get_logger(__name__, debug)
-    __log.debug('opt0=%s', opt0)
 
-    ctx.obj = {'opt0': opt0, 'debug': debug}
+    ctx.obj = {'debug': debug}
 
     subcmd = ctx.invoked_subcommand
     __log.debug('subcmd=%s', subcmd)
@@ -29,20 +29,48 @@ def cli(ctx, opt0, debug):
         print(ctx.get_help())
 
 
-@cli.command(help="cmd1")
-@click.argument('arg1', type=str)
-@click.option('--opt1', '-o1', 'opt1', type=str, default=None, help='opt')
+@cli.command(help="get")
 @click.option('--debug', '-d', 'debug', is_flag=True, default=False,
               help='debug flag')
+@click.option('--bus', '-b', 'bus', type=int, default=MCP3425.DEF_BUS,
+              help='I2C bus: default=%s' % MCP3425.DEF_BUS)
+@click.option('--addr', '-a', 'addr', type=int, default=MCP3425.DEF_ADDR,
+              help='I2C address: default=0x%02X' % MCP3425.DEF_ADDR)
+@click.option('--config', '-c', 'config', type=int, default=MCP3425.DEF_CONFIG,
+              help='configuration bits: default=%s' % bin(MCP3425.DEF_CONFIG))
+@click.option('--loop', '-l', 'loop', is_flag=True, default=False,
+              help='loop option')
+@click.option('--interval_sec', '-s', 'interval_sec', type=float, default=1.0,
+              help='loop interval [sec]: default=1.0')
+@click.option('--multiple', '-m', 'multiple', type=int, default=3,
+              help='multiple: default=3')
 @click.pass_obj
-def cmd1(obj, arg1, opt1, debug):
+def get(obj, bus, addr, config, loop, interval_sec, multiple, debug):
     """ cmd1 """
     __log = get_logger(__name__, obj['debug'] or debug)
-    __log.debug('obj=%s, opt1=%s, arg1=%s', obj, opt1, arg1)
+    __log.debug('obj=%s', obj)
+
+    pi = pigpio.pi()
+
+    adc = MCP3425(pi, bus, addr, config)
+
+    while True:
+        tm = time.localtime()
+        volts = adc.get()
+
+        print('%04d/%02d/%02d %02d:%02d:%02d, %.3f' % (
+            tm.tm_year, tm.tm_mon, tm.tm_mday,
+            tm.tm_hour, tm.tm_min, tm.tm_sec,
+            volts * multiple),
+              flush=True)
+
+        if not loop:
+            break
+        time.sleep(interval_sec)
 
 
-cli.add_command(cmd2)
-cli.add_command(cmd3)
+# cli.add_command(cmd2)
+# cli.add_command(cmd3)
 
 if __name__ == '__main__':
     cli(prog_name=__prog_name__)
