3e577a3e1ad26afa6cc979da1b7bbcd92467eaa5
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2021-11-13 17:54:48 +0900

    FIX: v_mon.sh

--- v_mon.sh
+++ v_mon.sh
@@ -8,7 +8,7 @@ MYDIR=`dirname $0`
 LOGDIR=$HOME/tmp
 LOGFILE=$LOGDIR/v.log
 
-CMDLINE="adc_mcp3425.sh watch"
+CMDLINE="$HOME/bin/adc_mcp3425.sh watch"
 
 if [ -f $LOGFILE ]; then
    mv -fv $LOGFILE $LOGFILE.1
